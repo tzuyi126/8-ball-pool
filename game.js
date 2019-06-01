48c1142d1542a1267ac1fc95dc63bf19ebfc2924
Legolas <legolas49494@gmail.com>
2019-06-01 21:16:03 +0800

    Getting start

---
+++ game.js
@@ -0,0 +1,11 @@
+// Initialize Phaser
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'canvas');
+// Define our global variable
+game.global = { score: 0 };
+// Add all the states
+game.state.add('boot', bootState);
+game.state.add('load', loadState);
+game.state.add('menu', menuState);
+game.state.add('play', playState);
+// Start the 'boot' state
+game.state.start('boot');
